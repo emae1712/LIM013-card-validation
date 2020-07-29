1af7454b0ded3e79086d4b31cd2e4c873a1045c8
emae1712 <melissa.aechegaray@gmail.com>
2020-07-29 12:48:00 -0500

    Validación y enmascaramiento de input tarjeta y HTML principal completo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,65 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+let credit = document.getElementById("tarjeta");
+let tarjetavalida = document.getElementById("tarjetavalida");
+let luhm = document.getElementById("luhm");
+
+//Muestra mensajes si la tarjeta es válida o no
+credit.addEventListener("keyup", () =>{
+
+    let creditCardNumber = credit.value; 
+    let algoritmo = validator.isvalid(creditCardNumber);
+    let mascara = validator.maskify(creditCardNumber);
+
+    tarjetavalida.innerHTML = mascara;
+    
+    if(creditCardNumber.length<16){
+        luhm.innerHTML = "Debe ser número de 16 dígitos"
+        if(algoritmo===true){
+        luhm.innerHTML = "Tarjeta válida";
+        }else{
+        luhm.innerHTML = "Tarjeta inválida";
+        }
+    }
+});
+
+//Coloca los inputs de texto en mayúsculas
+let titular = document.getElementById("titular");
+let nombre = document.getElementById("nombre");
+let apellido = document.getElementById("apellido");
+
+nombre.addEventListener("keyup", () =>{ 
+    nombre.value = nombre.value.toUpperCase();
+    a();
+});
+
+apellido.addEventListener("keyup", () =>{  
+    apellido.value = apellido.value.toUpperCase();
+    a();
+});
+
+function a(){
+    titular.value = nombre.value + " " + apellido.value
+};
+
+
+//Verifica los campos required y pasa a la siguiente página
+let botonPago = document.getElementById("botonPago");
+
+let validar = () =>{
+    let inputRequired= document.querySelectorAll("#formulario [required]");
+    
+    console.log(inputRequired);
+
+    for(let i = 0; i< inputRequired.length; i++ ){
+        if (inputRequired[i].value==''){
+            inputRequired[i].classList.add('error');
+        }else {
+            continue;
+        }
+    }
+
+};
+
+botonPago.addEventListener('click', validar);
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,36 @@
 const validator = {
-  // ...
-};
+  isvalid: function(a){
+     
+      //invirtiendo array (creamos variable para reducir nombre)
+      let inverso= Array.from(a).reverse();       
+      let total=0;
+      for(var i = 0; i < inverso.length; i++) {
+        if ((i % 2) !=0){
+          total+= inverso[i]*2%10+ parseInt(inverso[i]*2/10) ;
+        }else{
+          total+=parseInt(inverso[i],10);
+          }; 
+      }
+
+      //condición para que la suma sea múltiplo de 10
+      if(total%10===0 && total != 0){ 
+        return true;
+      }else{
+        return false;
+        }
+    //return a*2;*/
+  },
+
+  maskify: function (b){
+    let number = "";
+
+      if (b.length<=4){
+        number+= b.slice(-4);
+      }else{
+        number+= b.slice(0,-4).replace(/[0-9]/g,"#") + b.slice(-4);
+        }; 
+    return number;
+  }
+}
 
 export default validator;

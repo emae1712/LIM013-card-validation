e2390149762b014dc266784e39073055062db880
emae1712 <melissa.aechegaray@gmail.com>
2020-08-05 00:26:27 -0500

    detalles de la imagen de tarjeta de crédito de la segunda vista

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import validator from './validator.js';
 
 let credit = document.getElementById("tarjeta");
-let maskText = document.getElementById("maskText");
 let luhmText = document.getElementById("luhmText");
 
 let titular = document.getElementById("titular");
@@ -23,25 +22,25 @@ let header2 = document.getElementById("step3");
 
 //Coloca los inputs de texto en mayúsculas
 nombre.addEventListener("keyup", () =>{ 
-    nombre.value = nombre.value.toUpperCase();
-    a();
+  nombre.value = nombre.value.toUpperCase();
+  titularInput();
 });
 
 apellido.addEventListener("keyup", () =>{  
-    apellido.value = apellido.value.toUpperCase();
-    a();
+  apellido.value = apellido.value.toUpperCase();
+  titularInput();
 });
 
-function a(){
-    titular.value = nombre.value + " " + apellido.value;
+function titularInput(){
+  titular.value = nombre.value + " " + apellido.value;
 }
 
 //Edita el nombre de titular de la tarjeta que está deshabilitado por default
 check.addEventListener("click", ()=>{
     if (check.checked==true){
-        titular.disabled = false;
+      titular.disabled = false;
     }else{
-        titular.disabled=true;
+      titular.disabled=true;
     }
 })
 
@@ -51,20 +50,18 @@ let warning = () =>{
 
     let creditCardNumber = credit.value; 
     let algoritmo = validator.isvalid(creditCardNumber);
-    let mascara = validator.maskify(creditCardNumber);
     let mistake= true;
-   
-    maskText.innerHTML = mascara;
+  
     
     if( !/^[0-9]{0,16}$/.test(creditCardNumber) || creditCardNumber.length>16){
-        luhmText.textContent = "Procure que sean números de 16 dígitos";
-        luhmText.style.color = "red";
+      luhmText.textContent = "Procure que sean números de 16 dígitos";
+      luhmText.style.color = "red";
         
-        credit.classList.add('error');
+      credit.classList.add('error');
 
     }else if (creditCardNumber.length===16){
 
-        if(algoritmo===true){
+      if(algoritmo===true){
         luhmText.innerHTML = "<strong>Tarjeta válida </strong>";
         luhmText.style.color = "#00c2cb";
 
@@ -73,17 +70,17 @@ let warning = () =>{
         
         mistake= false;
 
-        }else{
+      }else{
         luhmText.textContent = "Tarjeta inválida, intente de nuevo";
         luhmText.style.color = "red";
 
         credit.required = false;
         credit.classList.add('error');
-        }
+      }
 
     }else{
-        luhmText.textContent ="";
-        credit.classList.remove('error');
+      luhmText.textContent ="";
+      credit.classList.remove('error');
     }
 
     return mistake;
@@ -118,6 +115,7 @@ let validar = () =>{
 function view(e){
     e.preventDefault();
     let validarReturn = validar();
+    let mascara = validator.maskify(credit.value);
 
     if(validarReturn==false){
     firstPage.classList.add("hide");
@@ -134,7 +132,7 @@ function view(e){
     }
 
    //Formulario de confirmación en la segunda vista
-    document.getElementById('cardResult').innerHTML = validator.maskify(credit.value);
+    document.getElementById('cardResult').innerHTML = mascara;
     document.getElementById('nameResult').innerHTML = titular.value;
     document.getElementById('expdateResult').innerHTML = "Vence " + mes.value + " / " + año.value;
     document.getElementById('emailResult').innerHTML = "<strong>Correo electrónico: </strong>" + email.value;
